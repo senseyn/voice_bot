8e69051dc67be0c61e761801e117d4d72734fc8f
senseyn <novik_alex06@mail.ru>
2025-06-12 22:50:23 +0300

    Assembling files-2

--- test3.py
+++ test3.py
@@ -1,14 +1,19 @@
 import asyncio
 import logging
-
+#=========СЕТЕВЫЕ БИБЛИОТЕКИ=========
+from aiogram.exceptions import TelegramNetworkError
+from aiohttp import ClientConnectorError
+#=========БИБЛИОТЕКИ СТАНДАРТ========
 from aiogram.enums import ChatAction
 from aiogram import Bot, Dispatcher, types, Router, F
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, BotCommand, BotCommandScopeDefault
 #==========ИМПОРТ МОИХ ФАЙЛОВ=========
 from handlers.welcome import print_start_banner
+from handlers.style_text import start_text_bot, stop_text_bot
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+logging.basicConfig(level=logging.INFO,
+                    format="\033[1;30;47m%(asctime)s - %(name)s - %(levelname)s - %(message)s\033[0m",
                     datefmt='%Y-%m-%d %H:%M:%S')  # Формат: ГГГГ-ММ-ДД ЧЧ:ММ:СС
 logger = logging.getLogger(__name__)
 
@@ -31,6 +36,8 @@ async def animate_text(message: types.Message):
     await message.delete()
     name = message.from_user.first_name
     text = f"Привет {name}"
+    await message.answer(text)
+
 
 #=============================================================
 @start_router.message(F.text == "/start")
@@ -68,16 +75,23 @@ async def callback_btn2(callback: types.CallbackQuery):
 
 
 async def main():
-    dp.include_router(start_router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await set_commands()  # УСТАНОВКА МЕНЮ КОМАНД
-    print("БОТ ЗАПУЩЕН")
-    print_start_banner() #ЗАПУСК ЗАСТАВКИ В ТЕРМИНАЛЕ ЛИНУКС
-    await dp.start_polling(bot)
+    try:
+        dp.include_router(start_router)
+        await bot.delete_webhook(drop_pending_updates=True)
+        await set_commands()  # УСТАНОВКА МЕНЮ КОМАНД
+        start_text_bot()
+        print_start_banner()
+        await dp.start_polling(bot) #СТАРТ БОТА
+    except (TelegramNetworkError, ClientConnectorError) as e:
+        print("\033[1;41m⚠️ Сетевая ошибка: не удалось подключиться к Telegram API\033[0m")
+        logging.error(f"[1;41mОшибка подключения: {e}")
+    except Exception as e:
+        print("\033[1;41m❌ Произошла непредвиденная ошибка.\033[0m")
+        logging.exception(f"Критическая ошибка в работе бота: {e}")
 
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("БОТ ОСТАНОВЛЕН")
+        stop_text_bot()
